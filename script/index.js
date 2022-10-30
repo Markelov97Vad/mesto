0924374c4f856c55b6d880521e9fdbc17ff9563f
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2022-10-30 22:04:35 +0300

    fix исправил замечания ревьюера, изменил код в index.js

--- script/index.js
+++ script/index.js
@@ -2,31 +2,32 @@ let popupEditButtonElem = document.querySelector('.profile__edit-button')
 let popupElem = document.querySelector('.popup');
 let popupCloseButtonElem = popupElem.querySelector('.popup__close-button');
 
-function openPopup () {
-  popupElem.classList.add('popup_opened');
-}
-
-function closePopup () {
-  popupElem.classList.remove('popup_opened');
-}
-popupEditButtonElem.addEventListener('click', openPopup);
-popupCloseButtonElem.addEventListener('click', closePopup);
-
 let nameElem = document.querySelector('.profile__title');
 let jobElem = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.form');
 let nameInput = formElement.querySelector('#name');
 let jobInput = formElement.querySelector('#job');
-let submitButtomElem = formElement.querySelector (".form__submit-button");
 
 
-function formSubmitHandler (evt) {
-  evt.preventDefault(); 
-  nameElem.textContent = nameInput.value;
-  jobElem.textContent = jobInput.value;
+function openPopup () {
+  popupElem.classList.add('popup_opened');
+  nameInput.value = nameElem.textContent;
+  jobInput.value = jobElem.textContent;
 }
 
+function closePopup () {
+  popupElem.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameElem.textContent = nameInput.value;
+  jobElem.textContent = jobInput.value;
+  closePopup ();
+}
+
+popupEditButtonElem.addEventListener('click', openPopup);
+popupCloseButtonElem.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
-submitButtomElem.addEventListener('click', closePopup);
 
