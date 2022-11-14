6e48f2eb9c95f317696ab026b3b891e5219e83ca
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2022-11-14 19:16:05 +0300

    feat: добавил кнопку удалить у карточки

--- script/index.js
+++ script/index.js
@@ -1,13 +1,42 @@
-let popupEditButtonElem = document.querySelector('.profile__edit-button')
-let popupElem = document.querySelector('.popup');
-let popupCloseButtonElem = popupElem.querySelector('.popup__close-button');
+const popupEditButtonElem = document.querySelector('.profile__edit-button')
+const popupElem = document.querySelector('.popup');
+const popupCloseButtonElem = popupElem.querySelector('.popup__close-button');
 
-let nameElem = document.querySelector('.profile__title');
-let jobElem = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('#name');
-let jobInput = formElement.querySelector('#job');
+const nameElem = document.querySelector('.profile__title');
+const jobElem = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.form');
+const nameInput = formElement.querySelector('#name');
+const jobInput = formElement.querySelector('#job');
 
+
+
+// карточки
+const initialCards = [
+  {
+    name: 'Му Кан Чай',
+    link: './images/image-card1.jpeg'
+  },
+  {
+    name: 'Багана',
+    link: './images/image-card.jpeg'
+  },
+  {
+    name: 'Чжанъе Данксиа',
+    link: './images/image-card3.jpeg'
+  },
+  {
+    name: 'Ваадху',
+    link: './images/image-card4.jpeg'
+  },
+  {
+    name: 'Тяньцзы',
+    link: './images/image-card5.jpg'
+  },
+  {
+    name: 'Мачу-Пикчу',
+    link: './images/image-card6.jpeg'
+  }
+];
 
 function openPopup () {
   popupElem.classList.add('popup_opened');
