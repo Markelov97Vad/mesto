77aa9527ac6fd2f4aea50e3dfa5165d05b4421e2
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2022-11-15 23:15:17 +0300

    feat: добавил скрипты добавления карточек, удаления, функция лайк

--- script/index.js
+++ script/index.js
@@ -14,7 +14,12 @@ const buttonClosePopup = popupNewCard.querySelector('.popup__close-button');
 const titleElem = document.querySelector('#title');
 const urlElem = document.querySelector('#url');
 const addCardButtonElem = document.querySelector('.profile__add-button');
-const createButtonElem = popupNewCard.querySelector('#create')
+//const createButtElem = popupNewCard.querySelector('.form__submit-button')
+
+const cardTemplateElem = document.querySelector('#cardTemplate').content;
+const cardElem = cardTemplateElem.querySelector('.element');
+// console.log(cardTemplateElem);
+// console.log(cardElem);
 
 // карточки
 const initialCards = [
@@ -44,61 +49,106 @@ const initialCards = [
   }
 ];
 
+
+// Добавление карточки
+
+const createCard = elemCard => {
+  elementsContainer.append(addNewCard(elemCard));
+}
+
+// создание карточки
+// const deletHandelcard = evt => {
+//   evt.target.closest('.element').remove();
+// }
+
+const addNewCard = elemCard => {
+  const copyCard = cardElem.cloneNode(true);
+
+  const title = copyCard.querySelector('.element__title');
+  const url = copyCard.querySelector('.element__image');
+  title.textContent = elemCard.name;
+  url.src = elemCard.link;
+
+  const deleteButCard = copyCard.querySelector('.element__delete-button');
+  deleteButCard.addEventListener('click', evt => {
+    evt.target.closest('.element').remove();
+  });
+
+  const likeButCard = copyCard.querySelector('.element__button');
+  likeButCard.addEventListener('click', evt => {
+    evt.target.closest('.element__button').classList.toggle('element__button_active');
+  });
+
+  return copyCard;
+}
+
+// обработчик событий
 const eventHandlerAddCard = (evt) => {
   evt.preventDefault();
-  console.log('submit');
-  // createCard({name: titleElem.value})
-  // titleElem.value = ''
+  createCard({name: titleElem.value, link: urlElem.value});
+  titleElem.value = '';
+  urlElem.value = '';
+  openCloseNewCardPopup();
 
 }
 
-popupNewCard.addEventListener('submit', eventHandlerAddCard);
 
-const createCard = (allCard) => {
-  elementsContainer.insertAdjacentHTML('afterbegin',
-  `<article class="element">
-    <button class="element__delete-button" type="button" aria-label="delete card"></button>
-    <img src="${allCard.link}" alt="Фото гор Тяньцзы" class="element__image">
-    <div class="element__info">
-      <h2 class="element__title">${allCard.name}</h2>
-      <button type="button" class="element__button" aria-label="mark like"></button>
-    </div>
-   </article>
-  `)
-}
 
-initialCards.forEach((allCard) => {
-  createCard(allCard);
+
+
+// const createCard = objactCard => {
+//   elementsContainer.insertAdjacentHTML('afterbegin',
+//   `<article class="element">
+//     <button class="element__delete-button" type="button" aria-label="delete card"></button>
+//     <img src="${objactCard.link}" alt="Фото гор Тяньцзы" class="element__image">
+//     <div class="element__info">
+//       <h2 class="element__title">${objactCard.name}</h2>
+//       <button type="button" class="element__button" aria-label="mark like"></button>
+//     </div>
+//    </article>
+//   `)
+// }
+
+// методом forEach перебираем каждый элемент массива карточек
+initialCards.forEach((elemCard) => {
+  createCard(elemCard);
+  //console.log(objactCard);
 })
 
-// функция для 'открытия' попапа редактирования профиля
-function openEditPopup () {
-  popupEditElem.classList.add('popup_opened');
+
+
+
+// функция для добавления и удаления класса попапу Ред. профиль (открыть,зарыть)
+function openCloseEditPopup () {
+  popupEditElem.classList.toggle('popup_opened');
   nameInput.value = nameElem.textContent;
   jobInput.value = jobElem.textContent;
 }
-// функция для 'закрытия' попапа редактирования профиля
-function closeEditPopup () {
-  popupEditElem.classList.remove('popup_opened');
-}
+
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameElem.textContent = nameInput.value;
   jobElem.textContent = jobInput.value;
-  closeEditPopup ();
+  openCloseEditPopup();
 }
-// функция для добавления и удаления класса попапу Новое место (открыть,зарыть)
-function openCloseNewCardPopup () {
-  popupNewCard.classList.toggle('popup_opened');
-};
+  // функция для добавления и удаления класса попапу Новое место (открыть,зарыть)
+  function openCloseNewCardPopup () {
+    popupNewCard.classList.toggle('popup_opened');
+  };
 
 
-popupEditButtonElem.addEventListener('click', openEditPopup); // кнопка "редактировать" открытия попапа Ред. профиль
-popupCloseButtonElem.addEventListener('click', closeEditPopup); // кнопка "X" попапа Ред. профиль
-formElement.addEventListener('submit', formSubmitHandler); // кнопка "сохранить" попапа Ред. профиль
-addCardButtonElem.addEventListener('click', openCloseNewCardPopup); // кнопка "+" открытия попапа Новое место
-buttonClosePopup.addEventListener('click', openCloseNewCardPopup); // кнопка "Х" попапа Новое место
-popupNewCard.addEventListener('submit', eventHandlerAddCard); // кнопка "создать" карточку
+  popupEditButtonElem.addEventListener('click', openCloseEditPopup); // кнопка "редактировать" открытия попапа Ред. профиль
+  popupCloseButtonElem.addEventListener('click', openCloseEditPopup); // кнопка "X" попапа Ред. профиль
+  formElement.addEventListener('submit', formSubmitHandler); // кнопка "сохранить" попапа Ред. профиль
+  addCardButtonElem.addEventListener('click', openCloseNewCardPopup); // кнопка "+" открытия попапа Новое место
+  buttonClosePopup.addEventListener('click', openCloseNewCardPopup); // кнопка "Х" попапа Новое место
+  popupNewCard.addEventListener('submit', eventHandlerAddCard); // кнопка "создать" карточку
+
+
+
+
+
+
 
 
