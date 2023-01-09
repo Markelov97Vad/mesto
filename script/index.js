ce8651f89bc7e5cd70902d49f4783ced536e19c9
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-01-09 19:15:59 +0300

    feat: wedpack

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 import { initialCards, objectSelectors, popupEditButtonElem, popupEditProfile, nameInput, jobInput, popupNewCard, addCardButtonElem } from "./constants.js"
-import FormValidator from "./FormValidator.js";
-import Card from "./Card.js";
+import FormValidator from "./components/FormValidator.js";
+import Card from "./components/Card.js";
 import Section from "./components/Section.js";
 import PopupWithImage from "./components/PopupWithImage.js";
 import PopupWithForm from "./components/PopupWithForm.js";
